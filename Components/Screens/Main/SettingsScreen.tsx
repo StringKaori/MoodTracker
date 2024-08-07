33ad4b80a23c80e5f3f06335bd911d7502eb89c8
Guilherme Leite <guilherme99leite@gmail.com>
2024-08-06 22:18:42 -0300

    Adding one line on settings screen.

--- Components/Screens/Main/SettingsScreen.tsx
+++ Components/Screens/Main/SettingsScreen.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ImageBackground} from 'react-native';
+import { StyleSheet, View, ImageBackground, Text, TouchableOpacity } from 'react-native';
 
 export default function SettingsScreen() {
   return (
@@ -8,13 +8,15 @@ export default function SettingsScreen() {
      resizeMode="cover"
      style={styles.background}>
       <StatusBar style="auto" />
-
+      
       <View style={styles.container}>
+        <TouchableOpacity style = {styles.button}>
+          <Text> Profile Preferences </Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -26,4 +28,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+  button: {
+    width: `100%`,
+    backgroundColor: `#EEEEEE`
+  }
 });
